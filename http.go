19a2a25be8d03bda155a685f107770858bf8a722
joseph0x45 <zozozozeph@gmail.com>
2026-04-12 08:43:12 +0000

    add ServeFile

--- http.go
+++ http.go
@@ -26,3 +26,11 @@ func ServeJS(r chi.Router, path, js string) {
 func ServeFS(r chi.Router, basePath string, fs embed.FS) {
 	r.Handle(basePath+"*", http.StripPrefix(basePath, http.FileServer(http.FS(fs))))
 }
+
+func ServeFile(r chi.Router, path, content, contentType string) {
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", contentType)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(content))
+	})
+}
